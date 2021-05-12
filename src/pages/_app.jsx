33dc5c9d9fbb20b3adea7c4abfa833b774eb927c
sato-taisei-3113 <sato.taisei.3113@members.co.jp>
2021-05-12 22:09:00 +0900

    🐛 スマホ表示バグの改善

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -8,7 +8,10 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <title>TODOリスト</title>
         <meta name="description" content="Let's start TODO-App" />
-        <meta name="viewport" content="width=device-width" />
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+        ></meta>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
